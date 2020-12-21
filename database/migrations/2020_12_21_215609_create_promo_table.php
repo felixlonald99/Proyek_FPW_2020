8ad5e68c2e3d7ed80ddfe3e4094782c9970dc453
fxaucky <auckyrh@gmail.com>
2020-12-21 22:11:17 +0700

    create table promo
    tabel promo

---
+++ database/migrations/2020_12_21_215609_create_promo_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promo', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_promo');
+            $table->integer('nominal_potongan');
+            $table->integer('minimal_transaksi');
+            $table->integer('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promo');
+    }
+}
